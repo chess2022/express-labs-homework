e709feec8a4b62464d44355849f74caf4549ed4d
chess2022 <lakeshore.hnh@gmail.com>
2022-04-02 17:48:27 -0700

    Tip calculator express application created

--- express-lab-2/server.js
+++ express-lab-2/server.js
@@ -5,4 +5,9 @@ app.get("/greeting/:name", function (req, res) {
   res.send("How are you doing today, " + req.params.name+`?`);
 });
 
+app.get("/tip/:total/:tipPercentage", function (req, res) {
+  let tip = parseInt(req.params.tipPercentage) / 100 * (req.params.total)
+  res.send(`Your tip should be $${tip}`)
+})
+
 app.listen(3000);
