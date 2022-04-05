c141871d146daa6514259c68adf6ccf7bbe0d1d8
chess2022 <lakeshore.hnh@gmail.com>
2022-04-04 19:13:27 -0700

    Take one down application created

---
+++ pass-it-around/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    let number = 99
+  res.send(`<h1>99 Bottles of Beer on the Wall</h1><br>
+    <a id="bottles" title="Click to count down"
+ href="http://localhost:3000/${number-1}" onclick="MyFunction();return false;">take one down, pass it around...</a>`);
+});
+
+
+app.get("/:number_of_bottles", function (req, res) {
+    let number = parseInt(req.params.number_of_bottles)
+    if (number > 0) {
+        res.send(
+          `<h2>${number} bottles of beer on the wall.</h2>` +
+            `<br>` + number + " bottles of beer on the wall, " +
+            `<br>` + number + " bottles of beer," +
+            `<br>` + `<a id="bottles" title="Click to count down"
+ href="http://localhost:3000/${number-1}" onclick="MyFunction();return false;">take one down, pass it around...</a>`
+        );
+    } else {
+        if (number === 0) {
+            res.send("There are no more bottles of beer left on the wall." + " " + `<a href = http://localhost:3000/>Start Over!</a>`)
+        }
+    }
+})
+
+app.listen(3000);
+
+
